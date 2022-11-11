f63d5918fd23891c923835842edd68d4520f6b78
DiNeez <dneez86@gmail.com>
2022-11-11 16:14:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# light weight and simple webpack-starter 
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './index.html'
+import './style.scss'
+
+const mult = (a, b) => a * b
+
+console.log(mult(2, 4))
+console.log(mult(5, 30))
